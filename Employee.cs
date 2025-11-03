d7eca3de17c3ec4b821bb8c1e7213a366c0c2950
skye0123 <aaronsuelen013@gmail.com>
2025-11-03 18:58:03 +0800

    Added Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,12 @@
+namespace InsightAttend
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string Role { get; set; }
+    }
+}
